3e70cf11606f5e0a7f02a72773a761ecc05f619b
olzhobaeva13 <81243296+olzhobaeva13@users.noreply.github.com>
2021-05-15 21:23:38 +0600

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,72 @@
+#задание 3
+students = [
+    {'name': 'John', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Andrew', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Marcus', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Agness', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Don', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Michael', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Jennifer', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Angela', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Eniston', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Albert', 'age': 33, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nash', 'age': 28, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Nicolas', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Alexey', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Martin', 'age': 22, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Gloria', 'age': 23, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Mikel', 'age': 24, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Susanne', 'age': 25, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Stevie', 'age': 26, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Mark', 'age': 18, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Johnathan', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aidin', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Mirbek', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aidana', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Atay', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Chyngyz', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Aigerim', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Jyldyz', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Emir', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Emirlan', 'age': 12, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nursultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aliaskar', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aktanbek', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Adyl', 'age': 14, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Janyl', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Meerim', 'age': 12, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Sultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+]
+#1
+ptn_names = []
+for i in students:
+    for j in i.values():
+        if j == 'python':
+            ptn_names.append(i.get('name'))
+js_names = []
+for i in students:
+    for j in i.values():
+        if j == 'javascript':
+            js_names.append(i.get('name'))
+jv_names = []
+for i in students:
+    for j in i.values():
+        if j == 'java':
+            jv_names.append(i.get('name'))
+
+courses ={
+    'python': ptn_names,
+    'javascript': jv_names,
+    'java': jv_names
+}
+print(courses)
+
+#2
+names = []
+ages = []
+for i in students:
+    for j in i.values():
+        names.append(i.get('name'))
+        ages.append(i.get('age'))
+        names_ages = dict(zip(names, ages))
+print(names_ages)
